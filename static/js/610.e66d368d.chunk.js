ad30d55260c6a1d095c2abe39f9fb3ba1bad653b
sebastian-deoldata <sebastian@deoldata.com>
2024-06-28 10:58:05 +0530

    28/06/24 10:57 AM

--- static/js/610.e66d368d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkveersky_support=self.webpackChunkveersky_support||[]).push([[610],{6610:(e,s,r)=>{r.r(s),r.d(s,{default:()=>p});var n=r(5043),t=r(579);class k extends n.Component{render(){return(0,t.jsx)(t.Fragment,{children:"Task Assignment works...!"})}}const p=k}}]);
-//# sourceMappingURL=610.e66d368d.chunk.js.map
